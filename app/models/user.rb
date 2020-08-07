81b8111219c6587081515e75798423bc288786bb
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-08-07 19:21:00 +0300

    Add other side of relationship from Comment model  to User and Post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
