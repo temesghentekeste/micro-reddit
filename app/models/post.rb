dbe693e004f04660c19eb9bd57ac9988b6584683
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-07 20:59:58 +0530

    Add validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :body, presence: true, length: {minimum: 10, maximum: 500}
+  validates :user_id, presence: true
+
 end
