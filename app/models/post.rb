73b83087438da9a1ec45afff9a4bcdf1895d2caa
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-07 21:19:44 +0530

    Add comment model with the user and post references and put validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :body, presence: true, length: {minimum: 10, maximum: 500}
+  validates :body, presence: true, length: { minimum: 10, maximum: 500 }
   validates :user_id, presence: true
-
 end
