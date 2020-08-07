30822e3832ef27dc46b069afe5c50873d5c3fab5
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-08-07 20:10:24 +0300

    Update README.md file and fixed linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10, maximum: 500 }
   validates :user_id, presence: true
